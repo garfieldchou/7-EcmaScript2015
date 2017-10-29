cf4e8026675b1e0ce950257077a2869998803899
shaudai <shaudai0402@hotmail.com>
2017-10-29 17:45:38 +0800

    achieve data privacy with block instead of IIFE

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let age6 = 23;
 name6 = 'Jane Miller';
 console.log(name6);
 */
-
+/*
 // ES5
 function driversLicence5(passedTest) {
 
@@ -50,9 +50,6 @@ driversLicence6(true);
 
 
 
-
-
-
 var i = 23;
 
 for (var i = 0; i < 5; i++) {
@@ -60,6 +57,32 @@ for (var i = 0; i < 5; i++) {
 }
 
 console.log(i);
+*/
+//////////////////////////////////
+// Lecture: Blocks and IIFEs
+
+// ES6
+{
+    const a = 1;
+    let b = 2;
+    var c = 3;
+}
+
+//console.log(a + b);
+console.log(c);
+
+
+// ES5
+(function(){
+    var c = 3;
+})();
+
+//console.log(c);
+
+
+
+
+
 
 
 
