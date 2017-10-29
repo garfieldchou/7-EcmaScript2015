e5243d332255c5dca1193a9263d5406867d603ed
shaudai <shaudai0402@hotmail.com>
2017-10-29 16:04:53 +0800

    declare variable outside of the block but in the function

--- script.js
+++ script.js
@@ -31,9 +31,11 @@ driversLicence5(true);
 // ES6
 function driversLicence6(passedTest) {
 
+    let firstName;
+    const yearOfBirth = 1990;
+
     if (passedTest) {
-        let firstName = 'John';
-        const yearOfBirth = 1990;
+        firstName = 'John';
     }
 
     console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
