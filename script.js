1fde05b51031795041305a97239ace5c02ca635c
shaudai <shaudai0402@hotmail.com>
2017-10-30 23:02:33 +0800

    the arrow function shares the lexical this keyword of surroundings

--- script.js
+++ script.js
@@ -148,9 +148,19 @@ var box5 = {
         });
     }
 }
-box5.clickMe();
+//box5.clickMe();
 
-
+const box6 = {
+    color: 'green',
+    position: 1,
+    clickMe: function() {
+        document.querySelector('.green').addEventListener('click', () => {
+            var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+            alert(str);
+        });
+    }
+}
+box6.clickMe();
 
 
 
