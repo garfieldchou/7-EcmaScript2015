6727b8b654ad2e2bce8fda06278316b4efc2f16d
shaudai <shaudai0402@hotmail.com>
2017-10-30 23:07:49 +0800

    this keyword point to the global object window
    alert result: This is box number undefined and it is undefined

--- script.js
+++ script.js
@@ -160,9 +160,19 @@ const box6 = {
         });
     }
 }
-box6.clickMe();
+//box6.clickMe();
 
-
+const box66 = {
+    color: 'green',
+    position: 1,
+    clickMe: () => {
+        document.querySelector('.green').addEventListener('click', () => {
+            var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+            alert(str);
+        });
+    }
+}
+box66.clickMe();
 
 
 
