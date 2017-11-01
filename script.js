8feaf73ade1b37ffaec87599ff2126f4ff806a33
garfield_chou <garfield_chou@asus.com>
2017-11-01 21:26:39 +0800

    Array.from() simply convert the node list to an array, and forEach can be used

--- script.js
+++ script.js
@@ -253,14 +253,16 @@ console.log(retirement);
 const boxes = document.querySelectorAll('.box');
 
 // ES5
+/*
 var boxesArr5 = Array.prototype.slice.call(boxes);
 boxesArr5.forEach(function(cur) {
     cur.style.backgroundColor = 'dodgerblue';
 });
+*/
 
-
-
-
+// ES6
+const boxesArr6 = Array.from(boxes);
+boxesArr6.forEach(cur => cur.style.backgroundColor = 'dodgerblue');
 
 
 
