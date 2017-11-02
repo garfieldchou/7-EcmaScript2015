5567d3b09fc30602c147a260d916b18760d70640
garfield_chou <garfield_chou@asus.com>
2017-11-03 07:14:01 +0800

    use a real array to save the arguments' content

--- script.js
+++ script.js
@@ -343,7 +343,12 @@ Array.from(all).forEach(cur => cur.style.color = 'purple');
 
 //ES5
 function isFullAge5() {
-    console.log(arguments);
+    //console.log(arguments);
+    var argsArr = Array.prototype.slice.call(arguments);
+
+    argsArr.forEach(function(cur) {
+        console.log((2016 - cur) >= 18);
+    });
 }
 
 isFullAge5(1990, 1999, 1965);
