0d8c419384654f8e9f72b7bbeffb179a71760be2
garfield_chou <garfield_chou@asus.com>
2017-11-03 06:52:47 +0800

    spread operator also works for node and node list, which is returned by querySelector/querySelectorAll

--- script.js
+++ script.js
@@ -331,6 +331,11 @@ const bigFamily = [...familySmith, 'Lily', ...familyMiller];
 console.log(bigFamily);
 
 
+const h = document.querySelector('h1');
+const boxes = document.querySelectorAll('.box');
+const all = [h, ...boxes];
+
+Array.from(all).forEach(cur => cur.style.color = 'purple');
 
 
 
