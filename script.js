6aae95cd34e795dc534a868045a339a3442bcc86
garfield_chou <garfield_chou@asus.com>
2017-11-04 12:55:59 +0800

    add 'limit' to the argument list in ES6 way

--- script.js
+++ script.js
@@ -371,9 +371,15 @@ function isFullAge5(limit) {
     });
 }
 
-isFullAge5(18, 1990, 1999, 1965);
+//isFullAge5(18, 1990, 1999, 1965);
 
 
+//ES6
+function isFullAge6(limit, ...years) {
+    years.forEach(cur => console.log((2016 - cur) >= limit));
+}
+
+isFullAge6(16, 1990, 1999, 1965, 2016, 1987);
 
 
 
