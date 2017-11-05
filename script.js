eb48d6eeb788684b43783111b18cf07db12ec801
garfield_chou <garfield_chou@asus.com>
2017-11-05 21:12:33 +0800

    in ES6, use extends and super() to inherit class

--- script.js
+++ script.js
@@ -526,14 +526,37 @@ johnAthlete5.calculateAge();
 johnAthlete5.wonMedal();
 
 
+//ES6
+class Person6 {
+    constructor (name, yearOfBirth, job) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.job = job;
+    }
 
+    calculateAge() {
+        var age = new Date().getFullYear() - this.yearOfBirth;
+        console.log(age);
+    }
+}
 
+class Athlete6 extends Person6 {
+    constructor(name, yearOfBirth, job, olympicGames, medals) {
+        super(name, yearOfBirth, job);
+        this.olympicGames = olympicGames;
+        this.medals = medals;
+    }
 
+    wonMedal() {
+        this.medals++;
+        console.log(this.medals);
+    }
+}
 
+const johnAthlete6 = new Athlete6('John', 1990, 'swimmer', 3, 10);
 
-
-
-
+johnAthlete6.wonMedal();
+johnAthlete6.calculateAge();
 
 
 
